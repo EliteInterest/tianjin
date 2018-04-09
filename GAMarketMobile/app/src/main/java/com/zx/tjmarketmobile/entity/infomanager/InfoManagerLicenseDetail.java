5a7143cb42f5ee0d491206fa6e33e1a940da414e
zsza <15902706892@163.com>
2018-04-09 11:10:33 +0800

    更新entity

--- GAMarketMobile/app/src/main/java/com/zx/tjmarketmobile/entity/infomanager/InfoManagerLicenseDetail.java
+++ GAMarketMobile/app/src/main/java/com/zx/tjmarketmobile/entity/infomanager/InfoManagerLicenseDetail.java
@@ -9,28 +9,35 @@ import java.io.Serializable;
 public class InfoManagerLicenseDetail implements Serializable {
 
     private String id;
+    private String business_area;
+    private String storehouse_address;
+    private String storehouse_type;
+    private long storehouse_start_date;
+    private long storehouse_end_date;
+    private String enterprise_mode;
+    private int is_cooked_food;
+    private int is_home_brewed;
+    private String business_category;
+    private String business_mode;
+    private String website;
+    private int is_central_kitchen;
+    private int is_distribution_unit;
+    private int is_vending;
+    private String status;
+    private int is_network;
     private String licNum;
-    private String typeName;
     private String enterpriseId;
     private String enterpriseName;
-    private String productedAddress;
+    private String address;
     private String legalPerson;
-    private String enterprisePrincipal;
-    private String enterpriseType;
-    private String categoryCode;
-    private String productedScope;
-    private long issueDate;
+    private long isSueDate;
     private long expireStartDate;
     private long expireEndDate;
     private String issueOrg;
-    private String contactPerson;
     private String contactInfo;
-    private String fax;
-    private String updateDescrible;
-    private String gmpNum;
-    private String certificationScope;
+    private String businessScope;
+    private String businessAddress;
     private String type;
-
 
     public String getId() {
         return id;
@@ -38,6 +45,150 @@ public class InfoManagerLicenseDetail implements Serializable {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getBusiness_area() {
+        return business_area;
+    }
+
+    public void setBusiness_area(String business_area) {
+        this.business_area = business_area;
+    }
+
+    public String getStorehouse_address() {
+        return storehouse_address;
+    }
+
+    public void setStorehouse_address(String storehouse_address) {
+        this.storehouse_address = storehouse_address;
+    }
+
+    public String getStorehouse_type() {
+        return storehouse_type;
+    }
+
+    public void setStorehouse_type(String storehouse_type) {
+        this.storehouse_type = storehouse_type;
+    }
+
+    public long getStorehouse_start_date() {
+        return storehouse_start_date;
+    }
+
+    public void setStorehouse_start_date(long storehouse_start_date) {
+        this.storehouse_start_date = storehouse_start_date;
+    }
+
+    public long getStorehouse_end_date() {
+        return storehouse_end_date;
+    }
+
+    public void setStorehouse_end_date(long storehouse_end_date) {
+        this.storehouse_end_date = storehouse_end_date;
+    }
+
+    public String getEnterprise_mode() {
+        return enterprise_mode;
+    }
+
+    public void setEnterprise_mode(String enterprise_mode) {
+        this.enterprise_mode = enterprise_mode;
+    }
+
+    public int getIs_cooked_food() {
+        return is_cooked_food;
+    }
+
+    public void setIs_cooked_food(int is_cooked_food) {
+        this.is_cooked_food = is_cooked_food;
+    }
+
+    public int getIs_home_brewed() {
+        return is_home_brewed;
+    }
+
+    public void setIs_home_brewed(int is_home_brewed) {
+        this.is_home_brewed = is_home_brewed;
+    }
+
+    public String getBusiness_category()
+    {
+        return business_category;
+    }
+
+    public void setBusiness_category(String business_category)
+    {
+        this.business_category = business_category;
+    }
+
+    public String getBusiness_mode()
+    {
+        return business_mode;
+    }
+
+    public void setBusiness_mode(String business_mode)
+    {
+        this.business_mode = business_mode;
+    }
+
+    public String getWebsite()
+    {
+        return website;
+    }
+
+    public void setWebsite(String website)
+    {
+        this.website = website;
+    }
+
+    public int getIs_central_kitchen()
+    {
+        return is_central_kitchen;
+    }
+
+    public void setIs_central_kitchen(int is_central_kitchen)
+    {
+        this.is_central_kitchen = is_central_kitchen;
+    }
+
+    public int getIs_distribution_unit()
+    {
+        return is_distribution_unit;
+    }
+
+    public void setIs_distribution_unit(int is_distribution_unit)
+    {
+        this.is_distribution_unit = is_distribution_unit;
+    }
+
+    public int getIs_vending()
+    {
+        return is_vending;
+    }
+
+    public void setIs_vending(int is_vending)
+    {
+        this.is_vending = is_vending;
+    }
+
+    public String getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(String status)
+    {
+        this.status = status;
+    }
+
+    public int getIs_network()
+    {
+        return is_network;
+    }
+
+    public void setIs_network(int is_network)
+    {
+        this.is_network = is_network;
     }
 
     public String getLicNum() {
@@ -48,14 +199,6 @@ public class InfoManagerLicenseDetail implements Serializable {
         this.licNum = licNum;
     }
 
-    public String getTypeName() {
-        return typeName;
-    }
-
-    public void setTypeName(String typeName) {
-        this.typeName = typeName;
-    }
-
     public String getEnterpriseId() {
         return enterpriseId;
     }
@@ -64,12 +207,22 @@ public class InfoManagerLicenseDetail implements Serializable {
         this.enterpriseId = enterpriseId;
     }
 
-    public String getProductedAddress() {
-        return productedAddress;
+    public String getEnterpriseName() {
+        return enterpriseName;
     }
 
-    public void setProductedAddress(String productedAddress) {
-        this.productedAddress = id;
+    public void setEnterpriseName(String enterpriseName) {
+        this.enterpriseName = enterpriseName;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
     }
 
     public String getLegalPerson() {
@@ -80,45 +233,28 @@ public class InfoManagerLicenseDetail implements Serializable {
         this.legalPerson = legalPerson;
     }
 
-    public String getEnterprisePrincipal() {
-        return enterprisePrincipal;
+    public long getIsSueDate() {
+        return isSueDate;
     }
 
-    public void setEnterprisePrincipal(String enterprisePrincipal) {
-        this.enterprisePrincipal = enterprisePrincipal;
+    public void setIsSueDate(long isSueDate) {
+        this.isSueDate = isSueDate;
     }
 
-    public String getEnterpriseType() {
-        return enterpriseType;
+    public long getExpireStartDate() {
+        return expireStartDate;
     }
 
-    public void setEnterpriseType(String enterpriseType) {
-        this.enterpriseType = enterpriseType;
+    public void setExpireStartDate(long expireStartDate) {
+        this.expireStartDate = expireStartDate;
     }
 
-
-    public String getEnterpriseName() {
-        return enterpriseName;
+    public long getExpireEndDate() {
+        return expireEndDate;
     }
 
-    public void setEnterpriseName(String enterpriseName) {
-        this.enterpriseName = enterpriseName;
-    }
-
-    public String getCategoryCode() {
-        return categoryCode;
-    }
-
-    public void setCategoryCode(String categoryCode) {
-        this.categoryCode = categoryCode;
-    }
-
-    public String getProductedScope() {
-        return productedScope;
-    }
-
-    public void setProductedScope(String productedScope) {
-        this.productedScope = productedScope;
+    public void setExpireEndDate(long expireEndDate) {
+        this.expireEndDate = expireEndDate;
     }
 
     public String getIssueOrg() {
@@ -129,14 +265,6 @@ public class InfoManagerLicenseDetail implements Serializable {
         this.issueOrg = issueOrg;
     }
 
-    public String getContactPerson() {
-        return contactPerson;
-    }
-
-    public void setContactPerson(String contactPerson) {
-        this.contactPerson = contactPerson;
-    }
-
     public String getContactInfo() {
         return contactInfo;
     }
@@ -145,36 +273,24 @@ public class InfoManagerLicenseDetail implements Serializable {
         this.contactInfo = contactInfo;
     }
 
-    public String getFax() {
-        return fax;
+    public String getBusinessScope()
+    {
+        return businessScope;
     }
 
-    public void setFax(String fax) {
-        this.fax = fax;
+    public void setBusinessScope(String businessScope)
+    {
+        this.businessScope = businessScope;
     }
 
-    public String getUpdateDescrible() {
-        return updateDescrible;
+    public String getBusinessAddress()
+    {
+        return businessAddress;
     }
 
-    public void setUpdateDescrible(String updateDescrible) {
-        this.updateDescrible = updateDescrible;
-    }
-
-    public String getGmpNum() {
-        return gmpNum;
-    }
-
-    public void setGmpNum(String gmpNum) {
-        this.gmpNum = gmpNum;
-    }
-
-    public String getCertificationScope() {
-        return certificationScope;
-    }
-
-    public void setCertificationScope(String certificationScope) {
-        this.certificationScope = certificationScope;
+    public void setBusinessAddress(String businessAddress)
+    {
+        this.businessAddress = businessAddress;
     }
 
     public String getType() {
@@ -184,35 +300,4 @@ public class InfoManagerLicenseDetail implements Serializable {
     public void setType(String type) {
         this.type = type;
     }
-
-    public long getIssueDate()
-    {
-        return issueDate;
-    }
-
-    public void setIssueDate(long issueDate)
-    {
-        this.issueDate = issueDate;
-    }
-
-    public long getExpireStartDate()
-    {
-        return expireStartDate;
-    }
-
-    public void setExpireStartDate(long expireStartDate)
-    {
-        this.expireStartDate = expireStartDate;
-    }
-
-    public long getExpireEndDate()
-    {
-        return expireEndDate;
-    }
-
-    public void setExpireEndDate(long expireEndDate)
-    {
-        this.expireEndDate = expireEndDate;
-    }
-
 }
